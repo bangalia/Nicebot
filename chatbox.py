d1ac259d8b825632a744f817ea8eb30a784bc13b
lotsofluck <anitabangali@yahoo.com>
2020-09-21 11:24:02 -0500

    Create chatbox.py

---
+++ chatbox.py
@@ -0,0 +1,5 @@
+from random import choice
+def get_bot_response(user_response):
+    bot_response_happy = ["Awesome!","Woohoo!"]
+    bot_response_sad = ["Sorry to hear that","I hope things perk up"]
+    
