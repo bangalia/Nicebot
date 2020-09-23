88ebad8c5f77e3ec1ee0ab006af1b0b2e0aa2e3e
lotsofluck <anitabangali@yahoo.com>
2020-09-22 20:28:03 -0500

    Spacing
    There was an issue with spacing

--- chatbox.py
+++ chatbox.py
@@ -1,25 +1,17 @@
 from random import choice
+
+
 def get_bot_response(user_response):
-    bot_response_good = ["Awesome!","Woohoo!","I'm really happy to hear that"]
-    bot_response_bad = ["Sorry to hear that","Oh no!","I hope things perk up"]
+    bot_response_good = ["Awesome!", "Great!", "I'm happy to hear that!"]
+    bot_response_bad = ["Sorry to hear that", "Oh no", "I hope things perk up"]
 
-    if user_response == "good":
+    if "good" in user_response:
         return choice(bot_response_good)
-    elif user_response == "bad": 
-        return choice(bot_response_bad) 
+    elif user_response == "bad":
+        return choice(bot_response_bad)
+    else:
+        return choice(bot_response_bad)
    
-
-def get_bot_cool(user_interest):
-    bot_cool_yes = ["A smile is the same in all languages"]
-    bot_cool_no = ["Okay, talk to you soon!"]
-
-    if user_interest == "yes":
-        return bot_cool_yes
-    else: 
-        return bot_cool_no 
-        
-        
-
 
 print("Hello and Welcome to Nicebot")
 print("Please enter how your day has been so far")
@@ -27,31 +19,10 @@ print("Please enter how your day has been so far")
 user_response = ""
 
 while True:
-    user_response = input("How has your day been so far?:")
-    
-    bot_response = get_bot_response
-    (user_response)
-    print(bot_response)
-    
-print("Would you like to hear something interesting?")
-print("Please enter yes or no")
+    user_response = str(input("How has your day been?"))
 
-user_interest = ""
-
-while True:
-    user_interest = input("Would you like to hear something cool?") 
-
-    if user_interest == "done":
+    if user_response == "done":
         break
 
-    bot_cool = get_bot_cool
-    (user_interest)
-    print(bot_cool)
-
-    
-
-
-
-
- 
-
+    bot_response = get_bot_response(user_response)
+    print(bot_response)
